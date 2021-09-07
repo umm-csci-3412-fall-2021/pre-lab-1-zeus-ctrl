506ec6cdeab87ada0acbffcb3b9b9286e5217904
zeus-ctrl <noah.constable@gmail.com>
2021-09-07 12:02:00 -0500

    Add comments.

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -4,7 +4,9 @@ toWrap=$1
 specifier=$2
 result=$3
 
+#Concat specifier with header and footer extensions
 specFooter="${specifier}_footer.html"
 specHeader="${specifier}_header.html"
 
+#Concatenate files in the desired resulting file.
 cat "$specHeader" "$toWrap" "$specFooter" >"$result"
