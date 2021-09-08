9fa5173a84fa022345a3568974da3a9157c7eeca
zeus-ctrl <const112@opportunity.morris.umn.edu>
2021-09-08 18:56:55 -0500

    Complete regex for first case.

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r0_input.txt
+awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r0_input.txt > r0_output.txt
